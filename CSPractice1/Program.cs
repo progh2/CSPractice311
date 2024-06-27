83ce61d18c1c81fc99e7a3642559ef8b91472387
Gihun Ham <ham@e-mirim.hs.kr>
2024-06-27 12:03:31 +0900

    #1 12-8 var 키워드

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -85,7 +85,9 @@ namespace CSPractice1
             Console.WriteLine(number--); // 12
             Console.WriteLine(--number); // 10
 
-
+            // #1 12-8 var 키워드
+            var varnumber = 20;
+            //var abc;  // X반드시 초기화 해줘야 함
 
         }
     }
