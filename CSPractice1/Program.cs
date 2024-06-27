a304441d5fe234d96d615c6d161a7308307304b5
Gihun Ham <ham@e-mirim.hs.kr>
2024-06-27 12:09:40 +0900

    #2 13-1. if 조건문

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -96,6 +96,18 @@ namespace CSPractice1
             // #1 12-10. 자료형 변환
             // 굉장히 중요
 
+            // #2 13-1. if 조건문
+            bool condition = true;
+            if (condition)
+            {
+                // some 
+            }else if(condition){
+                // some2
+            }
+            else
+            {
+                // other
+            }
         }
     }
 }
