80531a1f95f9f778639283348e76004d493bac71
Gihun Ham <ham@e-mirim.hs.kr>
2024-06-28 11:13:26 +0900

    #4 21-4. 인스턴스 변수

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -171,7 +171,22 @@ namespace CSPractice1
 
             // #4 21-3. 클래스 생성
 
+            // #4 21-4. 인스턴스 변수
+            User user1 = new User();
+            user1.name = "이미림";
+            user1.phoneNumber = "010-1234-5678";
 
+            Product p1 = new Product();
+            p1.name = "어른폰";
+            p1.price = 99999999;
+
+            Product p2 = new Product() {
+               price = 3000, name = "감자폰"
+            };
+            Product p3 = new Product()
+            {
+                name = "공짜폰"
+            };
         }
     }
 }
---
+++ CSPractice1/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPractice1
+{
+    internal class User
+    {
+        public string name;
+        public string phoneNumber;
+    }
+
+    class Product
+    {
+        public string name;
+        public int price;
+    }
+}
