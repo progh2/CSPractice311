91e23afc15a4a77d9298e0d2d13dea1bb0f759b2
Gihun Ham <ham@e-mirim.hs.kr>
2024-06-28 11:06:30 +0900

    #4 21-3. 클래스 생성

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace CSPractice1
 {
+    class FirstClass
+    {
+
+    }
+
     internal class Program
     {
+        class ThirdClass
+        {
+
+        }
+
         static void Main(string[] args)
         {
             // #1 12-1. 기본용어
@@ -158,6 +168,10 @@ namespace CSPractice1
             Console.WriteLine(Math.Min(42, 1));  // 1
             Console.WriteLine(Math.Max(42, 1));  // 42
             Console.WriteLine(Math.PI); // 3.141592~~
+
+            // #4 21-3. 클래스 생성
+
+
         }
     }
 }
