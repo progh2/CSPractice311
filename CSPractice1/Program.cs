c4d367032fd54a32be7ca1c4861f0eeb8f579d1b
Gihun Ham <ham@e-mirim.hs.kr>
2024-07-02 14:45:29 +0900

    #5 22-9. 값 복사와 참조 복사

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -76,6 +76,15 @@ namespace CSPractice1
 
     internal class Program
     {
+        class Test
+        {
+            public int value = 10;
+        }
+        static void Change(Test t)
+        {
+            t.value = 20;
+        }
+
         class ThirdClass
         {
 
@@ -277,6 +286,12 @@ namespace CSPractice1
             b.Width = 180;
             b.Height = 100;
             b.Height = -100;
+
+            // #5 22-9. 값 복사와 참조 복사
+            Test test = new Test();
+            test.value = 100;
+            Change(test);
+            Console.WriteLine(test.value); //  20 
 
         }
     }
